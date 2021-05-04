2a964111aa4fc5a897a9f218394e7048d7a8b2e8
MAD71241 <michele.deliso@gmail.com>
2021-05-04 19:26:17 +0200

    pulizia codice js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,32 +12,26 @@ burgerGen.addEventListener("click", function () {
     var ketchup = document.getElementById("ketchup").dataset;
     var ketchupCost = Number(ketchup.price);
     var ketchupCheck = document.getElementById("ketchup").checked;
-    console.log(ketchupCost);
     //prezzo e checkbox cheese
     var cheese = document.getElementById("cheese").dataset;
     var cheeseCost = Number(cheese.price);
     var cheeseCheck = document.getElementById("cheese").checked;
-    console.log(cheeseCost);
     //prezzo e checkbox tomato
     var tomato = document.getElementById("tomato").dataset;
     var tomatoCost = Number(tomato.price);
     var tomatoCheck = document.getElementById("tomato").checked;
-    console.log(tomatoCost);
     //prezzo e checkbox egg
     var egg = document.getElementById("egg").dataset;
     var eggCost = Number(egg.price);
     var eggCheck = document.getElementById("egg").checked;
-    console.log(eggCost);
     //prezzo e checkbox lettuce
     var lettuce = document.getElementById("lettuce").dataset;
     var lettuceCost = Number(lettuce.price);
     var lettuceCheck = document.getElementById("lettuce").checked;
-    console.log(lettuceCost);
     //prezzo e checkbox mustard
     var mustard = document.getElementById("mustard").dataset;
     var mustardCost = Number(mustard.price);
     var mustardCheck = document.getElementById("mustard").checked;
-    console.log(mustardCheck);
 
     var ingredients = [
         [document.getElementById("ketchup").checked, ketchupCost],
